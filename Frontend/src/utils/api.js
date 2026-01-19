e76c8fc47390a391e04335bf3059fd8a299d09df
Shubham23593 <shubhamdalvi775@gmail.com>
2026-01-19 22:27:45 +0530

    second  commit

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 // Create axios instance
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
-  headers: {
+  baseURL: process.env.REACT_APP_API_URL || 'https://togglenest-db.onrender.com/api',
+  headers: 
+  {
     'Content-Type': 'application/json',
   },
 });
